0389ee4e2659fda477045db4471fa15ab01d162f
awitaminosis <awitaminosis@mail.ru>
2025-03-09 09:55:10 +0300

    правка в доступности

--- places/states/feed_hedgehog.py
+++ places/states/feed_hedgehog.py
@@ -6,7 +6,7 @@ class FeedHedgehog(LocationCallbackQuery):
     can_reach = [
         ('tiger_home', t_go_to_tiger_home, 'inline', ''),
         ('hedgehog_home', t_go_to_hedgehog_home, 'inline', ''),
-        ('go_fishing', t_go_fishing, 'inline', ''),
+        # ('go_fishing', t_go_fishing, 'inline', ''),
         ('feed_hedgehog', t_feed_hedgehog, 'inline', Transitions.can_feed_hedgehog),
         # ('enter_forest', t_go_to_forest, 'inline', Transitions.can_go_to_forest),
     ]
