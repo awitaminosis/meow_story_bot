4c96b34d2499c6de1f549a45d80b79ed873d7fe9
awitaminosis <awitaminosis@mail.ru>
2025-11-15 16:39:11 +0300

    ruff for worms_dig

--- places/states/worms_dig.py
+++ places/states/worms_dig.py
@@ -1,4 +1,20 @@
-from places.states.base import *
+# from places.states.base import *
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+from places.states.conditions import Transitions
+from main import bot
+from helper.funcs import say, add_worms, maybe_eat_worms
+from main import logger
+from helper.texts import (
+    t_feed_hedgehog,
+    t_go_to_tiger_home,
+    t_dig_for_worms,
+    t_go_to_forest,
+)
+
+
+from places.states.base import LocationCallbackQuery
 
 
 class WormsDig(LocationCallbackQuery):
