d7608ca8a85fdf3c4dddeb46f35b9b438aed80d2
awitaminosis <awitaminosis@mail.ru>
2025-11-20 19:32:24 +0300

    extended ruff for enter_forest

--- places/states/enter_forest.py
+++ places/states/enter_forest.py
@@ -11,15 +11,15 @@ from places.states.conditions import Transitions
 
 class EnterForest(LocationCallbackQuery):
     location = "enter_forest"
-    can_reach = [
-        # ('tiger_home', t_go_to_tiger_home, 'inline', '', {}),
-        ("hedgehog_home", t_go_to_hedgehog_home, "inline", "", {}),
-        # ('go_fishing', t_go_fishing, 'inline', '', {},
-        # ('feed_hedgehog', t_feed_hedgehog, 'inline', Transitions.can_feed_hedgehog, {}),
-        ("visit_mouse", t_visit_mouse, "inline", Transitions.can_visit_mouse, {}),
-    ]
 
     def __init__(self, controller):
+        self.can_reach = [
+            # ('tiger_home', t_go_to_tiger_home, 'inline', '', {}),
+            ("hedgehog_home", t_go_to_hedgehog_home, "inline", "", {}),
+            # ('go_fishing', t_go_fishing, 'inline', '', {},
+            # ('feed_hedgehog', t_feed_hedgehog, 'inline', Transitions.can_feed_hedgehog, {}),
+            ("visit_mouse", t_visit_mouse, "inline", Transitions.can_visit_mouse, {}),
+        ]
         super().__init__(self.location, controller)
 
     async def handler(self, message: Message, state: FSMContext):
@@ -52,5 +52,5 @@ class EnterForest(LocationCallbackQuery):
         except Exception as e:
             logger.error(f"An error occurred: {e}")
 
-    async def filter(self, F):
+    async def filter(self, F):  # TODO
         return F.data == self.location
